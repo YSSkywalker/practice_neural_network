c204e915352bc3dccc1c8431fb23de0e1a4a28b1
YSSkywalker <yukimurasakiskywalker940716@gmail.com>
2020-06-18 16:40:55 +0900

    Increese nums of layers

--- python_scripts/simple_neural_network_model.py
+++ python_scripts/simple_neural_network_model.py
@@ -4,14 +4,17 @@ import matplotlib.pylab as plt
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+def identity_function(x):
+    return x
+
 # input layer => 1st hidden layer
 X = np.array([1.0, 0.5])
 W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
 B1 = np.array([0.1, 0.2, 0.3])
 
 # A = X・W + B
-A = np.dot(X, W1) + B1
-Z1 = sigmoid(A)
+A1 = np.dot(X, W1) + B1
+Z1 = sigmoid(A1)
 print(Z1)
 
 # 1st hidden layer => 2nd hidden layer
@@ -20,5 +23,14 @@ B2 = np.array([0.1, 0.2])
 
 # A = Z・W + B
 A2 = np.dot(Z1, W2) + B2
-Z2 = sigmoid(Z1)
+Z2 = sigmoid(A2)
 print(Z2)
+
+# 2nd hidden layer => output layer
+W3 = np.array([[0.1, 0.3], [0.2, 0.4]])
+B3 = np.array([0.1, 0.2])
+
+# A = Z・W + B
+A3 = np.dot(Z2, W3) + B3
+Z3 = identity_function(A3)
+print(Z3)
